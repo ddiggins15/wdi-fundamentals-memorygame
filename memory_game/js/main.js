a840e0e003f95fd2a19de503d41e6bc0a6e6a301
MisaMisa <Danielle@Danielles-MacBook-Pro.local>
2018-01-21 11:59:33 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+	card: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	card: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	card: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+	{
+	card: "king",
+	suit: "diamonds",	
+	cardImage: "images/king-of-diamonds.png"
+	}
+];
 
 var cardsInPlay = [];
 
@@ -15,8 +36,10 @@ var checkForMatch = function () {
 
 var flipCard = function (cardId) {
 	
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].card);
+	cardsInPlay.push(cards[cardId].card);
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
